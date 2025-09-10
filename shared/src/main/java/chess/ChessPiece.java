8c0d694f08ef6a877e27cbd2fbd761b77392da2e
kalojo <kalokjo@gmail.com>
2025-09-09 19:08:36 -0600

    Got one hardcoded move down

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 
 /**
  * Represents a single chess piece
@@ -53,6 +54,10 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        ChessPiece piece = board.getPiece(myPosition);
+        if (piece.getPieceType() == ChessPiece.PieceType.BISHOP) {
+            return List.of(new ChessMove(new ChessPosition(5,4), new ChessPosition(1,8), null));
+        }
         return new HashSet<ChessMove>();
     }
 }
