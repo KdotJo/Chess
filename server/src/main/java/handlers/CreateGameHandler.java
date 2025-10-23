de567d2f727f8aa43d34a871a0fc61f62b2e3023
kalojo <kalokjo@gmail.com>
2025-10-22 21:29:33 -0600

    fixed all the bugs in the create methods we are now done with create we will move on to join

--- server/src/main/java/handlers/CreateGameHandler.java
+++ server/src/main/java/handlers/CreateGameHandler.java
@@ -17,16 +17,17 @@ public class CreateGameHandler {
     }
     public void handleCreateGame (Context ctx) throws DataAccessException {
         try {
+            String authToken = ctx.header("authorization");
             CreateGameRequest request = ctx.bodyAsClass(CreateGameRequest.class);
-            if (request.authToken() == null || request.authToken().isEmpty()) {
-                ctx.status(401).json(Map.of("message", "Error: Unauthorized"));
+            if (authToken == null || authToken.isEmpty()) {
+                ctx.status(400).json(Map.of("message", "Error: Unauthorized"));
                 return;
             }
             if (request.gameName() == null || request.gameName().isEmpty()) {
                 ctx.status(400).json(Map.of("message", "Error: Game name is missing"));
                 return;
             }
-            CreateGameResult result = gameService.create(request);
+            CreateGameResult result = gameService.create(authToken, request);
             ctx.status(200).json(result);
         } catch (DataAccessException e) {
             ctx.status(401).json(Map.of("message", e.getMessage()));
--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -19,13 +19,12 @@ public class GameService {
         this.gameDao = gameDao;
     }
 
-    public CreateGameResult create(CreateGameRequest createGameRequest) throws DataAccessException {
-        if (authDao.getAuth(createGameRequest.authToken()) == null) {
+    public CreateGameResult create(String authToken, CreateGameRequest createGameRequest) throws DataAccessException {
+        if (authDao.getAuth(authToken) == null) {
             throw new DataAccessException("Error: Unauthorized");
         }
         int gameId = Math.abs(UUID.randomUUID().hashCode());
-        String username = authDao.getAuth(createGameRequest.authToken()).getUsername();
-
+        String username = authDao.getAuth(authToken).getUsername();
         ChessGame newGame = new ChessGame();
         GameData gameData = new GameData(gameId, username, null, createGameRequest.gameName(), newGame);
         gameDao.createGame(gameData);
