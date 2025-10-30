0b6d36c80e361eef907dee9968f13d57265bec71
kalojo <kalokjo@gmail.com>
2025-10-30 11:27:50 -0600

    currently have the files organized and made the general interface for all relevant functions

--- server/src/main/java/dataaccess/DataAccess.java
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -1,7 +1,37 @@
 package dataaccess;
 
 
+import dataaccess.memoryDAO.MemoryUserDAO;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.Collection;
 
 public interface DataAccess {
+//    user related interface requirements
 
+    void createUser(UserData userData) throws DataAccessException;
+
+    UserData getUser(String username) throws DataAccessException;
+
+    boolean usernameExists(String username) throws DataAccessException;
+
+//    game related interface requirements
+    void createGame(GameData gameData) throws DataAccessException;
+
+    GameData getGame(int gameID) throws DataAccessException;
+
+    Collection<GameData> listGames() throws DataAccessException;
+
+    void updateGame(int gameID, String whiteUsername, String blackUsername) throws DataAccessException;
+
+//    auth related interface requirements
+    String createAuth(String username) throws DataAccessException;
+
+    AuthData getAuth(String authToken) throws DataAccessException;
+
+    void deleteAuth(String authToken) throws DataAccessException;
+
+    void clear() throws DataAccessException;
 }
--- server/src/main/java/dataaccess/MySqlDAO/MySqlUserDAO.java
+++ server/src/main/java/dataaccess/MySqlDAO/MySqlUserDAO.java
@@ -1,4 +1,8 @@
 package dataaccess.MySqlDAO;
 
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+
+
 public class MySqlUserDAO {
 }
