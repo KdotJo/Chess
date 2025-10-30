c4e546e3d7d74fb3feb73855fa52b20cbcf8c383
kalojo <kalokjo@gmail.com>
2025-10-30 14:37:16 -0600

    I made the basis of the createUser function

--- server/src/main/java/dataaccess/MySqlDAO/MySqlUserDAO.java
+++ server/src/main/java/dataaccess/MySqlDAO/MySqlUserDAO.java
@@ -1,14 +1,43 @@
 package dataaccess.MySqlDAO;
+import java.sql.*;
 
+import dataaccess.DatabaseManager;
 import dataaccess.interfaces.UserDataAccess;
 import dataaccess.DataAccessException;
 import model.UserData;
+import org.mindrot.jbcrypt.BCrypt;
+
+import javax.xml.crypto.Data;
 
 
 public class MySqlUserDAO implements UserDataAccess {
     @Override
     public void createUser(UserData userData) throws DataAccessException {
+        var query = "SELECT * FROM users WHERE username = ?";
+        try (Connection connection = DatabaseManager.getConnection();
+             PreparedStatement queryStatement = connection.prepareStatement(query)) {
+            queryStatement.setString(1, userData.getUsername());
+            ResultSet queryResults = queryStatement.executeQuery();
+            if (queryResults.next()) {
+                throw new DataAccessException("Username Already Taken");
+            }
+            var createNewUser = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+            PreparedStatement insertUserStatement = connection.prepareStatement(createNewUser);
+            setUserValues(insertUserStatement, userData);
+            int results = insertUserStatement.executeUpdate();
+            if (results != 1) {
+                throw new DataAccessException("User Creation Failed");
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Unauthorized Error", e);
+        } ;
+    }
 
+    private void setUserValues (PreparedStatement values, UserData user) throws SQLException {
+        values.setString(1, user.getUsername());
+        String hashedPassword = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
+        values.setString(2, hashedPassword);
+        values.setString(3, user.getEmail());
     }
 
     @Override
