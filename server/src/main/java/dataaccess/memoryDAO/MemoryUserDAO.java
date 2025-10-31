fadc1513cfece4072f29190f3d3d3d943286dc8e
kalojo <kalokjo@gmail.com>
2025-10-30 19:25:35 -0600

    cleaned up this code

--- server/src/main/java/dataaccess/memoryDAO/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/memoryDAO/MemoryUserDAO.java
@@ -11,27 +11,17 @@ public class MemoryUserDAO implements UserDataAccess {
     HashMap<String, UserData> mockDB = new HashMap<>();
 
 
-    public void createUser (UserData userData) throws DataAccessException {
+    public void createUser (UserData userData) {
         // TODO: Store user in database
         // Check if username already exists
         // If not, add to mockDB
         // If yes, throw exception
-        String username = userData.getUsername();
-        if (usernameExists(username)) {
-            throw new DataAccessException("Error: already taken");
-        }
-        else {mockDB.put(username, userData);}
+        mockDB.put(userData.getUsername(), userData);
     }
 
     public UserData getUser (String username) {
         return mockDB.get(username);
      }
-
-
-    public boolean usernameExists (String username) {
-        return mockDB.containsKey(username);
-    }
-
 
     public void clear () {
         mockDB.clear();
