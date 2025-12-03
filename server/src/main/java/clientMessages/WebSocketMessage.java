8c610c0e5658c858efda97f89ddb0f5077dbcd3d
kalojo <kalokjo@gmail.com>
2025-12-03 10:32:12 -0700

    now building move

--- server/src/main/java/clientMessages/WebSocketMessage.java
+++ server/src/main/java/clientMessages/WebSocketMessage.java
@@ -1,7 +1,10 @@
 package clientMessages;
 
+import chess.ChessMove;
+
 public class WebSocketMessage {
     public String commandType;
     public String authToken;
     public int gameID;
+    public ChessMove move;
 }
